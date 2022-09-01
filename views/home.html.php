fd10db51c0ab90ef0f6a32b2ba91e06978e5f197
ChristopherSaury <saurychristopher@gmail.com>
2022-09-01 11:18:52 +0200

    homepage payment instructions

--- views/home.html.php
+++ views/home.html.php
@@ -34,8 +34,26 @@
         <h1 class="a-title">La mode aux meilleurs prix</h1>
         <h3>+ nouvelle collection de veste à découvrir</h3>
     </div>
-
 </div>
+<section id="homeAdd">
+    <div class="homePurchase">
+        <div>
+            <i class="fa-solid fa-percent"></i>
+            <h3>10% de réduction sur une commande</h3>
+            <p>10% de réduction sur une commande pour une souscription à la newsletter</p>
+        </div>
+        <div>
+            <i class="fa-solid fa-globe"></i>
+            <h3>Livraison sans frais </h3>
+            <p>Les livraisons sont offertes pour tout envoi dans le monde pour toutes commandes supérieures à 70€</p>
+        </div>
+        <div>
+            <i class="fa-regular fa-money-bill-1"></i>
+            <h3>Achetez maintenant payer plus tard</h3>
+            <p>Paiement en 3 versements ou Paiement en 30 jours sur les commandes en Europe</p>
+        </div>
+    </div>
+</section>
 
 
 

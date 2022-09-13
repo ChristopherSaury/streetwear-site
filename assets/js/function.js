224a6040ca253ea527f289949b8ab049790adf83
ChristopherSaury <saurychristopher@gmail.com>
2022-09-13 09:39:01 +0200

    forgotten console.log

--- assets/js/function.js
+++ assets/js/function.js
@@ -9,7 +9,6 @@ window.onload = () =>{
     const close = document.querySelector('.close');
     const links = document.querySelectorAll('.gallery-container a');
     const image = document.querySelector('.modal-content img');
-    console.log(image);
     
     for(let link of links){
         link.addEventListener("click", function(e){
--- function/Functions.php
+++ function/Functions.php
@@ -23,4 +23,19 @@ class Functions {
     public static function redirection(string $route) {
         return header("location:" . Config::BASE_URL . '/index.php?route=' . $route );
     }
+
+    public static function init_session() : bool{
+        if(!session_id()){
+            session_start();
+            session_regenerate_id();
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static function is_logged() :bool {
+        if(isset($_SESSION['firstname'])) return true;
+        else return false;
+    }
 }
